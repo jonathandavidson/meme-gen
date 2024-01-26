c53a068facf94eab01a8dbb1803f1790443b0084
Jonathan Davidson <jdavidson@netsplasher.com>
2024-01-26 16:08:58 -0600

    Add success test

--- test/meme-gen-create-test.mjs
+++ test/meme-gen-create-test.mjs
@@ -1,7 +1,10 @@
-import assert from 'assert';
+import LambdaTester from 'lambda-tester';
+import { handler } from '../src/meme-gen-create.mjs'
 
 describe('meme-gen-create', () => {
-  it('a test', () => {
-    assert.equal(true, true);
+  it('invokes successfullly', async () => {
+    await LambdaTester(handler)
+        .event({})
+        .expectResult()
   });
 });
